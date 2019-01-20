e5bebfe630520d6f26049fa8dd449bcc16ade72d
Liam Perlaki <lperlaki@icloud.com>
2019-01-20 21:47:02 +0100

    working reader

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,15 @@
 use std::io;
-use std::mem::size_of;
 
 mod utils;
 
-#[derive(Debug)]
-#[repr(C)]
-struct Vector {
-    x: f32,
-    y: f32,
-    z: f32,
-}
-
-#[repr(C)]
-struct WorldFileHeader {
-    level_seed: i32,
-    pos: Vector,
-    home: Vector,
-    yaw: f32,
-    directory_offset: u64,
-    name: [i8; 50],
-    version: i32,
-    hash: [i8; 36],
-    skycolors: [u8; 16],
-    goldencubes: i32,
-    _reserved: [i8; 40],
-}
-
-#[repr(C)]
-struct ColumnIndex {
-    x: i32,
-    z: i32,
-    chunk_offset: u64,
-}
-
-#[repr(C)]
-struct Chunk {
-    blocks: [[[u8; 16]; 16]; 16],
-    colors: [[[u8; 16]; 16]; 16],
-}
-
-#[repr(C)]
-struct Column {
-    chunks: [Chunk; 4],
-}
-
-use std::io::Cursor;
-use std::io::Read;
-
 fn main() -> io::Result<()> {
-    let mut c = utils::download_world("1541108087")?;
-    let header = utils::read_struct::<WorldFileHeader, _>(c.by_ref())?;
+    let mut world = utils::WorldFile::new("1541108087");
+    let header = world.header();
 
     println!("{}", header.level_seed);
-    println!("{}", c.position());
 
-    // let count = (header.directory_offset - size_of::<WorldFileHeader>() as u64)
-    //     / size_of::<Column>() as u64;
-    // println!("{}", count);
-
-    // let _columns = utils::read_struct::<[Column; 7784], _>(c.by_ref())?;
-    // reader.by_ref().take(
-    //     (header.directory_offset - size_of::<WorldFileHeader>() as u64)
-    //         - 7784 * size_of::<Column>() as u64,
-    // );
-    // let _indexes = utils::read_struct::<[ColumnIndex; 7784], _>(reader.by_ref())?;
+    let column_indexes = world.directory();
+    let col = column_indexes[0].column(&world);
 
     Ok(())
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,8 @@
 use libflate::gzip::Decoder;
 use reqwest;
-use std::io::Cursor;
-use std::io::{self, Read};
+use std::collections::HashMap;
+use std::io::{self, Cursor, Read, Seek, SeekFrom};
+use std::mem::size_of;
 
 pub fn read_struct<T, R: Read>(mut read: R) -> io::Result<T> {
     let num_bytes = ::std::mem::size_of::<T>();
@@ -23,4 +24,82 @@ pub fn download_world(_name: &'static str) -> io::Result<Cursor<Vec<u8>>> {
     let mut buffer = Vec::new();
     Decoder::new(req)?.read_to_end(&mut buffer)?;
     Ok(Cursor::new(buffer))
+}
+
+#[repr(C)]
+pub struct Vector {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+#[repr(C)]
+pub struct WorldFileHeader {
+    pub level_seed: i32,
+    pos: Vector,
+    home: Vector,
+    yaw: f32,
+    directory_offset: u64,
+    name: [i8; 50],
+    version: i32,
+    hash: [i8; 36],
+    skycolors: [u8; 16],
+    goldencubes: i32,
+    _reserved: [i8; 40],
+}
+
+#[repr(C)]
+pub struct ColumnIndex {
+    x: i32,
+    z: i32,
+    chunk_offset: u64,
+}
+
+#[repr(C)]
+pub struct Chunk {
+    blocks: [[[u8; 16]; 16]; 16],
+    colors: [[[u8; 16]; 16]; 16],
+}
+
+#[repr(C)]
+pub struct Column {
+    chunks: [Chunk; 4],
+}
+
+pub struct WorldFile {
+    cursor: Cursor<Vec<u8>>,
+}
+
+impl WorldFile {
+    pub fn new(name: &'static str) -> WorldFile {
+        WorldFile {
+            cursor: download_world(name).unwrap(),
+        }
+    }
+
+    pub fn header(&mut self) -> WorldFileHeader {
+        self.cursor.set_position(0);
+        read_struct::<WorldFileHeader, _>(self.cursor.by_ref()).unwrap()
+    }
+
+    pub fn directory(&mut self) -> Vec<ColumnIndex> {
+        let size = self.cursor.seek(SeekFrom::End(0)).unwrap();
+        let offset = self.header().directory_offset;
+        self.cursor.set_position(offset);
+        (offset..size)
+            .step_by(size_of::<ColumnIndex>())
+            .filter_map(|_| read_struct::<ColumnIndex, _>(self.cursor.by_ref()).ok())
+            .collect()
+    }
+
+    fn column(&mut self, pos: u64) -> Column {
+        self.cursor.set_position(pos);
+        read_struct::<Column, _>(self.cursor.by_ref()).unwrap()
+    }
+}
+
+impl ColumnIndex {
+    pub fn column(&self, world: &WorldFile) -> Column {
+        world.column(self.chunk_offset)
+    }
 }
